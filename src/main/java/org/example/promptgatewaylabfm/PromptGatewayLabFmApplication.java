5a88a746faebf7b5c36de2f6d5ae77b65f040e89
Fredrik Mohlen <fredrikmohlen@gmail.com>
2026-05-04 10:49:03 +0200

    initial commit

---
+++ src/main/java/org/example/promptgatewaylabfm/PromptGatewayLabFmApplication.java
@@ -0,0 +1,13 @@
+package org.example.promptgatewaylabfm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PromptGatewayLabFmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PromptGatewayLabFmApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/promptgatewaylabfm/PromptGatewayLabFmApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.promptgatewaylabfm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PromptGatewayLabFmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
