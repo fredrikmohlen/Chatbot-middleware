08795046a159119e545352762c1312bd6c403a4a
Fredrik Mohlen <fredrikmohlen@gmail.com>
2026-05-08 13:42:19 +0200

    remove redundant "public" before main. change variable from lowercase to uppercase in appl.properties

--- src/main/java/org/example/promptgatewaylabfm/PromptGatewayLabFmApplication.java
+++ src/main/java/org/example/promptgatewaylabfm/PromptGatewayLabFmApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PromptGatewayLabFmApplication {
 
-    public static void main(String[] args) {
+    static void main(String[] args) {
         SpringApplication.run(PromptGatewayLabFmApplication.class, args);
     }
 
